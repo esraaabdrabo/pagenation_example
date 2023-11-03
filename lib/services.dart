949f9b89cacbf0f801dca4570da0f45dd4c0f80b
esraaabdrabo <esraa@sindion.com>
2023-11-03 18:30:36 +0200

    complete getData method in Services

--- lib/services.dart
+++ lib/services.dart
@@ -1,9 +1,16 @@
 import 'package:dio/dio.dart';
+import 'package:pagenation_example/post_model.dart';
 
 abstract class Services {
   final Dio _dio = Dio()
     ..options.baseUrl = 'https://jsonplaceholder.typicode.com';
-  getData({String? pageNumber}) async {
-    await _dio.get('/posts?_page=$pageNumber');
+  Future<List<PostModel>> getData({String? pageNumber}) async {
+    var postsJson = await _dio.get('/posts?_page=$pageNumber');
+    List<Map<String, dynamic>> postsList = postsJson.data;
+    List<PostModel> posts = [];
+    for (var post in postsList) {
+      PostModel.fromJson(post);
+    }
+    return posts;
   }
 }
